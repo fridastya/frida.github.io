0011d06f32edd084929f415e549123061b749193
Fridastya <fridastya21@gmail.com>
2024-05-15 18:56:01 +0700

    Update index.php

--- perkantasbatamdb/index.php
+++ perkantasbatamdb/index.php
@@ -1,1 +1,17 @@
+<?php
 
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, "https://docs.google.com/spreadsheets/d/e/2PACX-1vTkUquiZ24FquwBiGhuQ6nvkGbQi6U0C4iw7m0igqY_TRMNyv97voc7fFQN3ez8yzUsJQ85a3vjsQYd/pub?gid=1096604120&single=true&output=csv");
+
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+     $response = curl_exec($ch);
+
+
+	curl_close($ch);
+
+	var_dump( explode( ',', $response ) );
+
+    
